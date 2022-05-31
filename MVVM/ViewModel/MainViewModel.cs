d88e254d5c71cc84d1aaab423d4338ca23f6442b
alexberkut98 <56170571+alexberkut98@users.noreply.github.com>
2022-05-31 18:29:04 +0300

    вв

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -5,8 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using Вторая_попытка_в_чат.Core;
 using Вторая_попытка_в_чат.MVVM.Model;
+<<<<<<< HEAD
+=======
+using Вторая_попытка_в_чат.MVVM.View;
+using Вторая_попытка_в_чат.Net;
+>>>>>>> 8936c0ed062ea16b682fc7576e313442f3eac154
 
 namespace Вторая_попытка_в_чат.MVVM.ViewModel
 {
@@ -17,6 +23,11 @@ namespace Вторая_попытка_в_чат.MVVM.ViewModel
         public ObservableCollection<UserModel> Users { get; set; }
         public RelayCommand SendCommand { get; set; }
         public RelayCommand ConnectToServerCommand { get; set; }
+
+        public RelayCommand MouseDown1 { get; set; }
+        public RelayCommand Mini1 { get; set; }
+        public RelayCommand ChangeStation { get; set; }
+        public RelayCommand Closing { get; set; }
 
         public ContactModel SelectedContact 
         { 
@@ -31,24 +42,13 @@ namespace Вторая_попытка_в_чат.MVVM.ViewModel
                 OnPropertyChanged();
             }
         }
-        private string _recipient;
         private ContactModel _selectedContact;
         private string _message;
 
         //Здесь будет храниться название текущего чата
         //All - общий чат.
         //UserName - чья та личка
-        public string Recipient 
-        { 
-            get
-            {
-                return _recipient;
-            }
-            set
-            {
-                value = _recipient;
-            }
-        }
+
         public string UserName { get; set; }
         public string Message 
         {
@@ -59,8 +59,21 @@ namespace Вторая_попытка_в_чат.MVVM.ViewModel
                 OnPropertyChanged();
             } 
         }
+        string UN="";
         public MainViewModel()
         {
+<<<<<<< HEAD
+=======
+            _server = new Server();
+            _server.ConnectedEvent += UserConnected;
+            _server.msgReceivedEvent += MessageReceived;
+            _server.userDisconnectEvent += RemoveUser;
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(UserName), o => !string.IsNullOrEmpty(UserName));
+            Mini1 = new RelayCommand(o => Minimise());
+            ChangeStation = new RelayCommand(o => WindowStateClick());
+            Closing = new RelayCommand(o => CloseWindow());
+
+>>>>>>> 8936c0ed062ea16b682fc7576e313442f3eac154
             Users = new ObservableCollection<UserModel>();
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
@@ -71,18 +84,88 @@ namespace Вторая_попытка_в_чат.MVVM.ViewModel
             Contacts.Add(current);
             OnPropertyChanged();
             SendCommand = new RelayCommand(o => Sending());
+            Contacts.Add(new ContactModel
+            {
+                UserName = "Said",
+                UserColor = "Coral",
+                Messages = new ObservableCollection<MessageModel>()
+            });
+            Contacts.Add(new ContactModel
+            {
+                UserName = "Murat",
+                UserColor = "Coral",
+                Messages = new ObservableCollection<MessageModel>()
+            });
+            Messages.Add(
+                new MessageModel
+                {
+                    UserName = "Said",
+                    Vis = "0",
+                    Target = "You",
+                    Time = DateTime.Now,
+                    Message = "Hello, User!"
+                }
+                );
+            Messages.Add(
+    new MessageModel
+    {
+        UserName = "Said",
+        Vis = "1",
+        Target = "All",
+        Time = DateTime.Now,
+        Message = "Long time no see!"
+    }
+    );
+            Messages.Add(
+    new MessageModel
+    {
+        UserName = "Murat",
+        Vis = "0",
+        Target = "You",
+        Time = DateTime.Now,
+        Message = "Hello, friend!!"
+    }
+    );
+            Messages.Add(
+    new MessageModel
+    {
+        UserName = "Murat",
+        Vis = "1",
+        Target = "All",
+        Time = DateTime.Now,
+        Message = "I need your help!!"
+    }
+    );
         }
-
+        public void AddMessage()
+        {
+      
+        }
         private void RemoveUser()
         {
 
         }
 
+        //Мало отправить сообщение на сервер, его еще надо отправить конкретному пользователю.
+        //Даже если имя адресата "All"
         private void Sending()
         {
             if(!string.IsNullOrEmpty(Message))
             {
+<<<<<<< HEAD
 
+=======
+                Messages.Add(
+                    new MessageModel
+                    {
+                        UserName = "You",
+                        Vis = "1",
+                        Target = SelectedContact.UserName,
+                        Time = DateTime.Now,
+                        Message = Message
+                    }
+                    );
+>>>>>>> 8936c0ed062ea16b682fc7576e313442f3eac154
             }
         }
 
@@ -91,7 +174,6 @@ namespace Вторая_попытка_в_чат.MVVM.ViewModel
         {
 
         }
-
         private void UserConnected()
         {
             
@@ -117,7 +199,34 @@ namespace Вторая_попытка_в_чат.MVVM.ViewModel
                 else
                     Contacts[i].UserColor = "Coral";
             }
+
+            for(int i=0;i<Messages.Count;i++)
+            {
+                if (Messages[i].UserName == SelectedContact.UserName && Messages[i].Target=="You" ||SelectedContact.UserName=="All" && Messages[i].Target == "All" || Messages[i].UserName == "You" && Messages[i].Target==SelectedContact.UserName)
+                    Messages[i].Vis = "1";
+                else
+                    Messages[i].Vis = "0";
+            }
             OnPropertyChanged();
+        }
+
+        
+        public void Minimise()
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        public void WindowStateClick()
+        {
+            //Если окно находится не в максимальном расширении - увеличить его.
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+         public void CloseWindow()
+        {
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ MVVM/ViewModel/WindowWorks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Вторая_попытка_в_чат.MVVM.ViewModel
+{
+    public class WindowWorks : Window
+    {
+        public void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
